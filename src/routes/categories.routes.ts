94144f83b91d85a9aba68d9fc423fab0129afdf7
thefalked <giulianocrivelli@hotmail.com>
2021-08-10 19:28:29 -0400

    created route and model for categories
    added uuid and post route to create a new category

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+
+import { Category } from "../model/Category";
+
+const categoriesRoutes = Router();
+
+const categories: Category[] = [];
+
+categoriesRoutes.post("/", (req, res) => {
+  const { name, description } = req.body;
+
+  const category = new Category();
+
+  Object.assign(category, { name, description, created_at: new Date() });
+
+  categories.push(category);
+
+  res.status(201).json({ category });
+});
+
+export { categoriesRoutes };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from "express";
 
+import { categoriesRoutes } from "./routes/categories.routes";
+
 const app = express();
 
 const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
+
+app.use("/categories", categoriesRoutes);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
