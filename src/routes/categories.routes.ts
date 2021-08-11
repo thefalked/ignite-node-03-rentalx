c0c0666bcc84fa600809726b4b6ef264198b742d
thefalked <giulianocrivelli@hotmail.com>
2021-08-11 19:17:01 -0400

    creating Categories Repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,21 +1,16 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repository/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
-  const category = new Category();
+  categoriesRepository.create({ name, description });
 
-  Object.assign(category, { name, description, created_at: new Date() });
-
-  categories.push(category);
-
-  res.status(201).json({ category });
+  res.status(201).send();
 });
 
 export { categoriesRoutes };
