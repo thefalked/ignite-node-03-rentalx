ab9f6d06e38b5fad20c4eaee2423074447c4b9f5
thefalked <giulianocrivelli@hotmail.com>
2021-08-11 19:25:20 -0400

    validating create category

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,14 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
   const { name, description } = req.body;
 
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+  if (categoryAlreadyExists) {
+    res.status(409).send({
+      error: "Category already exists",
+    });
+  }
+
   categoriesRepository.create({ name, description });
 
   res.status(201).send();
