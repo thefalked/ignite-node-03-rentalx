e45048ea6721d3362b7d8d87d229623474b1d288
thefalked <giulianocrivelli@hotmail.com>
2021-08-16 22:34:46 -0400

    adding controller and changing category to useCases

---
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -0,0 +1,13 @@
+import { CategoriesRepository } from "../../repository/CategoriesRepository";
+import { CreateCategoryController } from "./CreateCategoryController";
+import { CreateCategoryUseCase } from "./CreateCategoryUseCase";
+
+const categoriesRepository = new CategoriesRepository();
+
+const createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository);
+
+const createCategoryController = new CreateCategoryController(
+  createCategoryUseCase
+);
+
+export { createCategoryController };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,14 @@
 import { Router } from "express";
 
 import { CategoriesRepository } from "../modules/cars/repository/CategoriesRepository";
-import { CreateCategoryService } from "../modules/cars/services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
 
 const categoriesRoutes = Router();
 const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post("/", (req, res) => {
-  const { name, description } = req.body;
-
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-
-  res.status(201).send();
-});
+categoriesRoutes.post("/", (req, res) =>
+  createCategoryController.handle(req, res)
+);
 
 categoriesRoutes.get("/", (req, res) => {
   const categories = categoriesRepository.list();
