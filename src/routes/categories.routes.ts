688a7798b9c0bd9065f41435417ab855008d672e
thefalked <giulianocrivelli@hotmail.com>
2021-08-24 03:11:57 -0400

    added multer, configured and tested on insominia

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
+const upload = multer({
+  dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (req, res) =>
   createCategoryController.handle(req, res)
@@ -12,5 +16,13 @@ categoriesRoutes.post("/", (req, res) =>
 categoriesRoutes.get("/", (req, res) =>
   listCategoriesController.handle(req, res)
 );
+
+categoriesRoutes.post("/import", upload.single("file"), (req, res) => {
+  const { file } = req;
+
+  console.log(file);
+
+  return res.send(file);
+});
 
 export { categoriesRoutes };
