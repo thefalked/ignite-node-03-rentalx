531669dbc98380f35732f39c30e1eff6c4874e7a
thefalked <giulianocrivelli@hotmail.com>
2021-08-29 02:45:55 -0400

    creating docs with swagger

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,16 @@
 import express from "express";
+import swaggerIU from "swagger-ui-express";
 
 import { router } from "./routes";
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 const PORT = process.env.PORT || 3333;
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerIU.serve, swaggerIU.setup(swaggerFile));
 
 app.use(router);
 
