cb913951a867005c7a6773fd57ff80ce677419dc
thefalked <giulianocrivelli@hotmail.com>
2021-09-10 22:54:07 -0400

    fixing docker network issue

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,3 +1,16 @@
-import { createConnection } from "typeorm";
+import { createConnection, getConnectionOptions } from "typeorm";
 
-createConnection();
+interface IOptions {
+  host: string;
+}
+
+getConnectionOptions().then((options) => {
+  const newOptions = options as IOptions;
+
+  // This option must be EXACTLY has defined in the container_name of the database service
+  newOptions.host = "database_ignite";
+
+  createConnection({
+    ...options,
+  });
+});
