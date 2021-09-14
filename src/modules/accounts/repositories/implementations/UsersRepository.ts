ccb3757ed3ad8ca94b03611c630371d92c75d34f
thefalked <giulianocrivelli@hotmail.com>
2021-09-14 14:57:50 -0400

    creating users repository

---
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,29 @@
+import { Repository } from "typeorm";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUserRepository";
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  async create({
+    name,
+    username,
+    password,
+    email,
+    driver_license,
+  }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      username,
+      password,
+      email,
+      driver_license,
+    });
+
+    await this.repository.save(user);
+  }
+}
+
+export { UsersRepository };
