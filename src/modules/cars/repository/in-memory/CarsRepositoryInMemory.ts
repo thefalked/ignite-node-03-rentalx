46af8228aa4ea1f50737b09e06a4e7cbff8b7b28
thefalked <giulianocrivelli@hotmail.com>
2021-11-29 20:33:12 -0400

    feat: adding cars repository, controller and routes

--- src/modules/cars/repository/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repository/in-memory/CarsRepositoryInMemory.ts
@@ -32,8 +32,8 @@ class CarsRepositoryInMemory implements ICarsRepository {
     return car;
   }
 
-  async findByLicensePlate(licensePlate: string): Promise<Car> {
-    return this.cars.find((car) => car.license_plate === licensePlate);
+  async findByLicensePlate(license_plate: string): Promise<Car> {
+    return this.cars.find((car) => car.license_plate === license_plate);
   }
 }
 
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,8 +2,10 @@ import { container } from "tsyringe";
 
 import { UsersRepository } from "@modules/accounts/infra/repositories/UsersRepository";
 import { IUsersRepository } from "@modules/accounts/repositories/IUserRepository";
+import { CarsRepository } from "@modules/cars/infra/typeorm/repositories/CarRepository";
 import { CategoriesRepository } from "@modules/cars/infra/typeorm/repositories/CategoriesRepository";
 import { SpecificationsRepository } from "@modules/cars/infra/typeorm/repositories/SpecificationsRepository";
+import { ICarsRepository } from "@modules/cars/repository/ICarsRepository";
 import { ICategoriesRepository } from "@modules/cars/repository/ICategoriesRepository";
 import { ISpecificationsRepository } from "@modules/cars/repository/ISpecificationsRepository";
 
@@ -21,3 +23,5 @@ container.registerSingleton<IUsersRepository>(
   "UsersRepository",
   UsersRepository
 );
+
+container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
