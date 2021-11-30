eabb1e41afcfe64d51a1736b256ad476846e6ba5
thefalked <giulianocrivelli@hotmail.com>
2021-11-30 14:41:39 -0400

    feat: adding middleware to check if user is admin

---
+++ src/shared/infra/http/middlewares/ensureAdmin.ts
@@ -0,0 +1,25 @@
+import { NextFunction, Request, Response } from "express";
+
+import { UsersRepository } from "@modules/accounts/infra/repositories/UsersRepository";
+import { AppError } from "@shared/errors/AppError";
+
+export async function ensureAdmin(
+  req: Request,
+  _res: Response,
+  next: NextFunction
+): Promise<void> {
+  const { id } = req.user;
+
+  if (!id) {
+    throw new AppError("You are not authorized to access this route", 401);
+  }
+
+  const usersRepository = new UsersRepository();
+  const user = await usersRepository.findById(id);
+
+  if (!user.isAdmin) {
+    throw new AppError("You are not authorized to perform this action", 401);
+  }
+
+  return next();
+}
--- src/shared/infra/http/routes/categories.routes.ts
+++ src/shared/infra/http/routes/categories.routes.ts
@@ -5,6 +5,7 @@ import uploadConfig from "@config/upload";
 import { CreateCategoryController } from "@modules/cars/useCases/createCategory/CreateCategoryController";
 import { ImportCategoryController } from "@modules/cars/useCases/importCategory/ImportCategoryController";
 import { ListCategoriesController } from "@modules/cars/useCases/listCategories/ListCategoriesController";
+import { ensureAdmin } from "@shared/infra/http/middlewares/ensureAdmin";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
 const categoriesRoutes = Router();
@@ -15,14 +16,20 @@ const createCategoryController = new CreateCategoryController();
 const importCategoryController = new ImportCategoryController();
 const listCategoriesController = new ListCategoriesController();
 
-categoriesRoutes.use(ensureAuthenticated);
-categoriesRoutes.post("/", createCategoryController.handle);
+categoriesRoutes.post(
+  "/",
+  ensureAuthenticated,
+  ensureAdmin,
+  createCategoryController.handle
+);
 
-categoriesRoutes.get("/", listCategoriesController.handle);
+categoriesRoutes.get("/", ensureAuthenticated, listCategoriesController.handle);
 
 categoriesRoutes.post(
   "/import",
   uploadCategoriesImport.single("file"),
+  ensureAuthenticated,
+  ensureAdmin,
   importCategoryController.handle
 );
 
