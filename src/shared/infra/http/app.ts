7ac378efc9fe1df1037bc76a51a06ff30f385396
thefalked <giulianocrivelli@hotmail.com>
2021-12-14 04:12:38 -0400

    feat: configuring supertest

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,35 @@
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
+import swaggerIU from "swagger-ui-express";
+
+import { AppError } from "@shared/errors/AppError";
+import createConnection from "@shared/infra/typeorm";
+
+import "@shared/container";
+
+import swaggerFile from "../../../swagger.json";
+import { router } from "./routes";
+
+createConnection();
+const app = express();
+
+app.use(express.json());
+
+app.use("/api-docs", swaggerIU.serve, swaggerIU.setup(swaggerFile));
+
+app.use(router);
+
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`,
+  });
+});
+
+export { app };
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,37 +1,5 @@
-import express, { NextFunction, Request, Response } from "express";
-import "express-async-errors";
-import swaggerIU from "swagger-ui-express";
-
-import { AppError } from "@shared/errors/AppError";
-import createConnection from "@shared/infra/typeorm";
-
-import "@shared/container";
-
-import swaggerFile from "../../../swagger.json";
-import { router } from "./routes";
-
-createConnection();
-const app = express();
+import { app } from "./app";
 
 const PORT = process.env.PORT || 3333;
-
-app.use(express.json());
-
-app.use("/api-docs", swaggerIU.serve, swaggerIU.setup(swaggerFile));
-
-app.use(router);
-
-app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
-  if (err instanceof AppError) {
-    return res.status(err.statusCode).json({
-      message: err.message,
-    });
-  }
-
-  return res.status(500).json({
-    status: "error",
-    message: `Internal server error - ${err.message}`,
-  });
-});
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
