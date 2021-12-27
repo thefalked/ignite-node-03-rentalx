58c7a8b7354a275f3a73d01c4b278c4211da9ad0
thefalked <giulianocrivelli@hotmail.com>
2021-12-27 18:40:09 -0400

    fix: imports

--- src/modules/cars/infra/typeorm/repositories/CategoriesRepository.ts
+++ src/modules/cars/infra/typeorm/repositories/CategoriesRepository.ts
@@ -3,7 +3,8 @@ import { getRepository, Repository } from "typeorm";
 import {
   ICategoriesRepository,
   ICreateCategoryDTO,
-} from "../../../repository/ICategoriesRepository";
+} from "@modules/cars/repositories/ICategoriesRepository";
+
 import { Category } from "../entities/Category";
 
 class CategoriesRepository implements ICategoriesRepository {
