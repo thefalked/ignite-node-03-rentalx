81667111b074e088ae7918878a5a7ddde2b84bf9
thefalked <giulianocrivelli@hotmail.com>
2022-01-30 03:02:17 -0400

    feat: creating UserToken table

---
+++ src/shared/infra/typeorm/migrations/1643521913770-CreateUserToken.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateUserToken1643521913770 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "user_tokens",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+            isPrimary: true,
+          },
+          {
+            name: "refresh_token",
+            type: "varchar",
+          },
+          {
+            name: "user_id",
+            type: "uuid",
+          },
+          {
+            name: "expires_date",
+            type: "timestamp",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FK_user_token_id",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("user_tokens");
+  }
+}
