c942b1df1f04b8b2b24710e851924b3e9959e76d
thefalked <giulianocrivelli@hotmail.com>
2022-02-11 10:33:01 -0400

    feat: adding user profile with avatar url

--- src/shared/infra/http/routes/users.routes.ts
+++ src/shared/infra/http/routes/users.routes.ts
@@ -3,6 +3,7 @@ import multer from "multer";
 
 import uploadConfig from "@config/upload";
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
+import { ProfileUserController } from "@modules/accounts/useCases/profileUser/ProfileUserController";
 import { UpdateUserAvatarController } from "@modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 import { ensureAuthenticated } from "@shared/infra/http/middlewares/ensureAuthenticated";
 
@@ -12,9 +13,10 @@ const uploadAvatar = multer(uploadConfig);
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
+const profileUserController = new ProfileUserController();
 
+usersRoutes.get("/profile", ensureAuthenticated, profileUserController.handle);
 usersRoutes.post("/", createUserController.handle);
-
 usersRoutes.patch(
   "/avatar",
   ensureAuthenticated,
