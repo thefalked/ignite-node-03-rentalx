9015093f92d080e3232a5f6d10e37cc025268719
thefalked <giulianocrivelli@hotmail.com>
2022-02-25 14:43:48 -0400

    refactor: removed unused parameter on typeorm config

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,6 +1,6 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
-export default async (host = "database_ignite"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
