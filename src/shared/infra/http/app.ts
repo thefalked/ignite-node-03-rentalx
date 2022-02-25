db185c8a8ee30cb2a34be1c287c45bae8aa96c90
thefalked <giulianocrivelli@hotmail.com>
2022-02-25 18:55:44 -0400

    build: added cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,4 +1,5 @@
 import "dotenv/config";
+import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerIU from "swagger-ui-express";
@@ -21,6 +22,8 @@ app.use("/api-docs", swaggerIU.serve, swaggerIU.setup(swaggerFile));
 
 app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
+
+app.use(cors());
 
 app.use(router);
 
