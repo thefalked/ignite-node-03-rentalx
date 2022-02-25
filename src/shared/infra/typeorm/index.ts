ef097110df7fcc55a40c3fcc5f5a14e0a7a485ba
thefalked <giulianocrivelli@hotmail.com>
2022-02-25 14:34:57 -0400

    fix: database config

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -5,7 +5,6 @@ export default async (host = "database_ignite"): Promise<Connection> => {
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
           ? "rentx_test"
